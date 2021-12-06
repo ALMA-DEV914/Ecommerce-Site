be75148aa7a93aa55c6ba910b9d82168b9ebc2cf
ALMA_DEV914 <aungonalna58@gamil.com>
2021-12-06 14:13:10 -0800

    add product columns

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,50 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id columns
+    id: {
+       //use the special sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      //this is the equivalent of SQL's 'NOT NULL' option
+      allowNull: false,
+      //this is the equivalent of SQL's 'NOT NULL' option
+      primaryKey: true,
+      //turn on auto increment
+      autoIncrement: true
+    },
+    //define a product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    },
+    //define price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    //define stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      //if alloNull is  set to false, we can run our data through validators before creating the data
+      validate: {
+        isNumeric: true
+      }
+    },
+    // define category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      // refer to the category data model id
+      references: {
+        model: "category",
+        key: "id"
+      }
+    }
   },
   {
     sequelize,
